6ea4c651ca5851d6dd329608711a16015fb29b89
Harry-hhj <13818658603@163.com>
2021-05-01 01:06:42 +0800

    add lesson7-hw1

--- lesson-3-homework/hw2/app.js
+++ lesson-3-homework/hw2/app.js
@@ -4,15 +4,21 @@ $(document).ready(function () {
 
         //获取表单的系列化数据。这会生成一个A=valueA&B=valueB这种形式的字符串。
         let formData = $('form').serialize().substr(5)
-        let li = $("<li>"+formData+"</li>")
+        let li = $("<li><icon><icon/>"+formData+"</li>")
+        li.css({cursor: "pointer"})
         $('.tasksBoard').css({"display": "block"})
         $('.tasksBoard ul').append(li)
+        $('#myinput').val("")
         e.preventDefault()
     });
 
     $('#clear').click(function () {
-            $('.tasksBoard').css({"display": "none"})
-            $('.tasksBoard ul').find("li").remove();
-        }
-    )
+        $('.tasksBoard').css({"display": "none"})
+        $('.tasksBoard ul').find("li").remove()
+    })
+
+    $('.tasksBoard ul li').click(function () {
+        console.log("click")
+        this.remove()
+    })
 });
